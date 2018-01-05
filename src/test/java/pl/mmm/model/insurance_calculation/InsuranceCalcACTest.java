3cbe0380403190cc29d62e74da5760edb0036ba2
yakuza105 <yakuza105@gmail.com>
2018-01-05 08:46:56 +0100

    Insurance AC tests

--- src/test/java/pl/mmm/model/insurance_calculation/InsuranceCalcACTest.java
+++ src/test/java/pl/mmm/model/insurance_calculation/InsuranceCalcACTest.java
@@ -2,11 +2,42 @@ package pl.mmm.model.insurance_calculation;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
 
 public class InsuranceCalcACTest {
+    private double estimatedCarPrice = 1000.0;
+    private double baseCost = 500;
+    private double percentage = 0.05;
+    private InsuranceCalc calc = new InsuranceCalcAC();
+
     @Test
-    public void calculateInsuranceCosts() throws Exception {
+    public void calculatePercentCosts() throws Exception {
+        InsuranceFactors factors = new InsuranceFactors.Builder()
+                .estimatedCarPrice(estimatedCarPrice)
+                .build();
+
+        BigDecimal result = calc.calculateInsuranceCosts(factors);
+
+        assertEquals(percentage * estimatedCarPrice, result.doubleValue(), 0.01);
     }
 
-}
+
+    @Test
+    public void calculateACCosts() throws Exception {
+        InsuranceFactors factors = new InsuranceFactors.Builder()
+                .estimatedCarPrice(estimatedCarPrice)
+                .baseCosts(baseCost)
+                .build();
+
+        BigDecimal result = calc.calculateInsuranceCosts(factors);
+
+        assertEquals(baseCost+ (percentage* estimatedCarPrice), result.doubleValue(), 0.01);
+    }
+
+
+    }
+
+
+
